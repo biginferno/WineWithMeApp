29a6a95a6cdabf3f310ba3c123469949b37bdcd9
Ryan Romero <romeroryan01@gmail.com>
2018-12-24 13:25:06 -0800

    First Iteration - Barcode Scanner Complete

---
+++ app/src/main/java/com/example/ryanromero/winewithme/showType.java
@@ -0,0 +1,13 @@
+package com.example.ryanromero.winewithme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class showType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_type);
+    }
+}
